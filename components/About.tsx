f23f248d4085ad3aecfa3ffcabd9107d1b8a7b55
Saurabh-p11 <saurabh.p@navtech.io>
2025-07-08 13:10:22 +0530

    refactor: remove About component

--- components/About.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function About() {
-  return (
-    <div className="h-full flex justify-center items-center bg-gray-100">
-      <h2 className="text-3xl font-bold">About Section (Screen 2)</h2>
-    </div>
-  );
-}
